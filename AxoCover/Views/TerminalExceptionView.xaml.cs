4ca7cc3565334dbe186095bf855ae9ec7508346d
axodox <axodox@outlook.com>
2017-03-25 13:32:12 +0100

    Added fatal error reporting

---
+++ AxoCover/Views/TerminalExceptionView.xaml.cs
@@ -0,0 +1,32 @@
+﻿using AxoCover.Common.Events;
+using AxoCover.ViewModels;
+using System;
+
+namespace AxoCover.Views
+{
+  /// <summary>
+  /// Interaction logic for TerminalExceptionView.xaml
+  /// </summary>
+  public partial class TerminalExceptionView : View<TerminalExceptionViewModel>, IDialog
+  {
+    public TerminalExceptionView()
+    {
+      InitializeComponent();
+    }
+
+    public string Title
+    {
+      get
+      {
+        return AxoCover.Resources.TerminalException;
+      }
+    }
+
+    public event EventHandler<EventArgs<bool?>> ClosingDialog;
+
+    public void OnClosing()
+    {
+      ViewModel.RestartCommand.Execute(null);
+    }
+  }
+}
