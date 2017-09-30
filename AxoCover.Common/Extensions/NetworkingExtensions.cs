6a5a1153c09a6ca140edda27d7e474cb79b27691
axodox <axodox@outlook.com>
2017-09-30 22:50:41 +0200

    Use named pipes to communicate with test runner

--- AxoCover.Common/Extensions/NetworkingExtensions.cs
+++ AxoCover.Common/Extensions/NetworkingExtensions.cs
@@ -5,6 +5,7 @@ using System.Net;
 using System.Net.NetworkInformation;
 using System.Net.Sockets;
 using System.ServiceModel;
+using System.ServiceModel.Channels;
 
 namespace AxoCover.Common.Extensions
 {
@@ -13,58 +14,22 @@ namespace AxoCover.Common.Extensions
     public static readonly TimeSpan NetworkTimeout = TimeSpan.MaxValue;
     public static readonly TimeSpan SessionTimeout = TimeSpan.MaxValue;
 
-    public static NetTcpBinding GetServiceBinding()
+    public static Binding GetServiceBinding()
     {
-      var binding = new NetTcpBinding(SecurityMode.None)
+      var binding = new NetNamedPipeBinding(NetNamedPipeSecurityMode.None)
       {
         MaxReceivedMessageSize = int.MaxValue,
         ReceiveTimeout = TimeSpan.MaxValue,
         SendTimeout = NetworkTimeout,
         OpenTimeout = NetworkTimeout,
         CloseTimeout = NetworkTimeout
-      };
-      binding.ReliableSession.Enabled = true;
-      binding.ReliableSession.InactivityTimeout = SessionTimeout;
-      binding.ReliableSession.Ordered = true;
+      };      
       return binding;
     }
 
-    public static Uri GetServiceAddress(int minPort = 49152, int maxPort = IPEndPoint.MaxPort)
+    public static Uri GetServiceAddress()
     {
-      var ipProperties = IPGlobalProperties.GetIPGlobalProperties();
-
-      var usedPorts = new List<int>();
-      usedPorts.AddRange(ipProperties
-        .GetActiveTcpConnections()
-        .Select(p => p.LocalEndPoint.Port));
-      usedPorts.AddRange(ipProperties
-        .GetActiveTcpListeners()
-        .Select(p => p.Port));
-
-      int port;
-      for (port = minPort; port < maxPort; port++)
-      {
-        if (usedPorts.Contains(port)) continue;
-
-        try
-        {
-          var tcpListener = new TcpListener(IPAddress.Loopback, port);
-          tcpListener.Start();
-          tcpListener.Stop();
-          break;
-        }
-        catch
-        {
-
-        }
-      }
-
-      if (port == maxPort)
-      {
-        throw new Exception("All ports in the specified segment are in use.");
-      }
-
-      return new Uri($"net.tcp://{IPAddress.Loopback}:{port}");
+      return new Uri($"net.pipe://localhost/axoCover-runner/" + Guid.NewGuid().ToString());
     }
   }
 }
