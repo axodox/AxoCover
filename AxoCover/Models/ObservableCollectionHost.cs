82b6769bfb80dc0b0bc6577d0c11a1fff1a4890b
axodox <axodox@outlook.com>
2018-02-24 14:35:54 +0100

    Fixed issue with keyboard navigation in test explorer where selection would get stuck in collapsed namespaces

---
+++ AxoCover/Models/ObservableCollectionHost.cs
@@ -0,0 +1,48 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using System.Collections.Specialized;
+
+namespace AxoCover.Models
+{
+  public class ObservableCollectionHost<T> : IEnumerable<T>, INotifyCollectionChanged
+  {
+    public event NotifyCollectionChangedEventHandler CollectionChanged;
+
+    private INotifyCollectionChanged _target;
+    public INotifyCollectionChanged Target
+    {
+      get
+      {
+        return _target;
+      }
+      set
+      {
+        if(_target != null)
+        {
+          _target.CollectionChanged -= OnTargetCollectionChanged;
+        }
+        _target = value;
+        CollectionChanged?.Invoke(this, new NotifyCollectionChangedEventArgs(NotifyCollectionChangedAction.Reset));
+        if (_target != null)
+        {
+          _target.CollectionChanged += OnTargetCollectionChanged;
+        }
+      }
+    }
+
+    private void OnTargetCollectionChanged(object sender, NotifyCollectionChangedEventArgs e)
+    {
+      CollectionChanged?.Invoke(this, e);
+    }
+
+    public IEnumerator<T> GetEnumerator()
+    {
+      return (_target as IEnumerable<T> ?? new T[0]).GetEnumerator();
+    }
+
+    IEnumerator IEnumerable.GetEnumerator()
+    {
+      return GetEnumerator();
+    }
+  }
+}
